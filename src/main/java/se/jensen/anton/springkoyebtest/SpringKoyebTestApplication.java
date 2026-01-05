ba4cc8079661eab3438c1e5d37e405963eef26bd
AntonHenriksson <henriksson.anton@gmail.com>
2026-01-05 14:29:04 +0100

    Initial commit

---
+++ src/main/java/se/jensen/anton/springkoyebtest/SpringKoyebTestApplication.java
@@ -0,0 +1,13 @@
+package se.jensen.anton.springkoyebtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKoyebTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKoyebTestApplication.class, args);
+	}
+
+}
---
+++ src/main/java/se/jensen/anton/springkoyebtest/Test.java
@@ -0,0 +1,34 @@
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Test {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    private String name;
+
+    public Test() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
